a2cf478cb22a9161893e76e02bb89fafea63200c
ayushman101 <amansinghayush@gmail.com>
2024-06-27 22:47:39 +0530

    token in localstorage used for fetching courses

--- src/components/Discourses/Discourse.jsx
+++ src/components/Discourses/Discourse.jsx
@@ -1,22 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import "./Discourse.css";
 import DiscoursesCard from "./DiscoursesCard";
+import isTokenExpired from "../middlewares/isTokenExpired";
+
 
 const Discourse = () => {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
+
+    const tokenKey ="prasthan_yatna_jwt"
+
+    if(!isTokenExpired(tokenKey)){
+  
+    const token = localStorage.getItem(tokenKey)
+  
     fetch('http://localhost:3001/api/course/courses',{
 			method: 'GET',
 			headers:  {
 				Accept:`application/json`,
-				Authorization:`Bearer ` + `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NjdjNWZmYmU3YjlkMDY3N2JiZTNiZjAiLCJlbWFpbCI6ImpqQGdtYWlsLmNvbSIsInVzZXJuYW1lIjoiSkoiLCJpYXQiOjE3MTk0MjcwNjcsImV4cCI6MTczNDk3OTA2N30.S3-aXIKbq3rZRGeERn34I9BASOqsh9ZJavH9zyCdIoE`,
+				Authorization:`Bearer ` + token,
 			}, 
     }) 
       .then(response => response.json())
       .then(data => setCourses(data))
       .catch(error => console.error('Error fetching courses:', error));
+
+    }else {
+    alert("Please Login or Register")
+    navigate('/login');
+    }
+
+
   }, []);
+
+
 
   return (
     <>
